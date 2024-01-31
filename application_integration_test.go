a3a6672ed2f48416ccc1d7f617e13ced02b7cc53
Kay Freyer <kay.freyer@icloud.com>
2024-01-31 14:03:04 +0100

    application_test in root

--- application_integration_test.go
+++ application_integration_test.go
@@ -1,38 +1,19 @@
-package main
+package poker
 
 import (
-	"app"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
-	"os"
 	"testing"
 )
-
-func createTempFile(t testing.TB, initialData string) (*os.File, func()) {
-	t.Helper()
-	tmpfile, err := os.CreateTemp("", "db")
-
-	if err != nil {
-		t.Fatalf("could not create temp file %v", err)
-	}
-
-	tmpfile.Write([]byte(initialData))
-
-	removeFile := func() {
-		tmpfile.Close()
-		os.Remove(tmpfile.Name())
-	}
-	return tmpfile, removeFile
-}
 
 func TestRecordingWinsAndRetrievingThem(t *testing.T) {
 	database, cleanDatabase := createTempFile(t, `[]`)
 	defer cleanDatabase()
-	store, err := app.NewFileSystemPlayerStore(database)
+	store, err := NewFileSystemPlayerStore(database)
 	assertNoError(t, err)
 
-	server := app.NewPlayerServer(store)
+	server := NewPlayerServer(store)
 
 	player := "Pepper"
 
@@ -49,28 +30,4 @@ func TestRecordingWinsAndRetrievingThem(t *testing.T) {
 
 	assertStatusCode(t, response.Code, http.StatusOK)
 	assertResponseBody(t, response.Body.String(), "3")
-}
-
-func assertResponseBody(t *testing.T, got, want string) {
-	t.Helper()
-
-	if got != want {
-		t.Errorf("got = \"%v\"; want \"%v\"", got, want)
-	}
-
-}
-
-func assertStatusCode(t *testing.T, got, want int) {
-	t.Helper()
-
-	if got != want {
-		t.Errorf("got = \"%v\"; want \"%v\"", got, want)
-	}
-}
-
-func assertNoError(t *testing.T, err error) {
-	t.Helper()
-	if err != nil {
-		t.Fatalf("didn't expect an error but got one, %v", err)
-	}
 }
