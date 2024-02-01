94816d3515f1898aaaa41a2e45c3d23951d9a165
Kay Freyer <kay.freyer@icloud.com>
2024-02-01 14:30:53 +0100

    add unit tests to test cli and add dummies

--- poker/CLI_test.go
+++ poker/CLI_test.go
@@ -11,15 +11,41 @@ import (
 const PlayerPrompt = "Please enter the number of players: "
 
 func TestCLI(t *testing.T) {
-	t.Run("Get the number of players from the user", func(t *testing.T) {
-		stdout := bytes.Buffer{}
-		in := strings.NewReader("7\n")
-		cli := poker.NewCLI(in, &stdout)
+	var (
+		dummyOut = &bytes.Buffer{}
+		dummyIn  = strings.NewReader("")
+	)
+	t.Run("Prompt for getting the number of players", func(t *testing.T) {
+		stdout := &bytes.Buffer{}
+		cli := poker.NewCLI(dummyIn, stdout)
 
 		cli.PromptForPlayers()
 
 		got := stdout.String()
 		want := PlayerPrompt
+		if got != want {
+			t.Errorf(`got = %v; want %v`, got, want)
+		}
+	})
+	t.Run("Get the number of players from the user", func(t *testing.T) {
+		in := strings.NewReader("7\n")
+		cli := poker.NewCLI(in, dummyOut)
+
+		got, err := cli.GetNbrOfPlayers()
+		poker.AssertNoError(t, err)
+
+		want := 7
+		if got != want {
+			t.Errorf(`got = %v; want %v`, got, want)
+		}
+	})
+	t.Run("Get a winner of a game", func(t *testing.T) {
+		in := strings.NewReader("Chris wins\n")
+		cli := poker.NewCLI(in, dummyOut)
+
+		got := cli.GetWinner()
+
+		want := "Chris"
 		if got != want {
 			t.Errorf(`got = %v; want %v`, got, want)
 		}
